af30f940e5324f07ed0e210d8da097e89a093361
Jeff Chan <cp4c5ism@gmail.com>
2024-03-06 07:44:31 -0500

    problem 3 plots

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -45,4 +45,56 @@ ax.set_title("Earth's orbit and 400 Ducrosa's orbit")
 ax.set_aspect('equal')
 ax.grid()
 fig.legend(loc = 2)
+
+
+
+#problem 3
+IMPath = 'https://raw.githubusercontent.com/cchan011/Astro/main/HW2/IM-1.txt'
+IMdata = np.genfromtxt(IMPath, skip_header=114, skip_footer= 68,delimiter=',', names=['JDTDB','CalendarDate','X','Y','Z','VX','VY','VZ'])
+MoonPath = 'https://raw.githubusercontent.com/cchan011/Astro/main/HW2/Moon.txt'
+Mdata = np.genfromtxt(MoonPath, skip_header=50, skip_footer= 68,delimiter=',', names=['JDTDB','CalendarDate','X','Y','Z','VX','VY','VZ'])
+
+fig2 = plt.figure()
+ax1 = plt.subplot()
+
+ax1.plot(0, 0, 'bo', ms=1, label='Earth')
+ax1.plot(Mdata['X'], Mdata['Y'], marker ='',ms=3, c='black', label='Moon')
+ax1.plot(IMdata['X'], IMdata['Y'], marker ='',ms=3, c='purple', label='IM-1')
+ax1.set_xlabel('X [km]')
+ax1.set_ylabel('Y [km]')
+ax1.set_title('IM-1 path from Earth to Moon')
+ax1.set_aspect('equal')
+ax1.grid()
+fig2.legend(loc = 2)
+
+fig3 = plt.figure()
+ax2 = plt.subplot()
+
+ax2.plot(0, 0, 'bo', ms=1, label='Earth')
+ax2.plot(Mdata['X'], Mdata['Z'], marker ='',ms=3, c='black', label='Moon')
+ax2.plot(IMdata['X'], IMdata['Z'], marker ='',ms=3, c='purple', label='IM-1')
+ax2.set_xlabel('X [km]')
+ax2.set_ylabel('Z [km]')
+ax2.set_title('IM-1 path from Earth to Moon, XZ axes')
+ax1.grid()
+fig3.legend(loc = 2)
+
+fig4 = plt.figure()
+ax3 = plt.subplot()
+
+speed = np.sqrt(IMdata['VX']**2+IMdata['VY']**2+IMdata['VZ']**2)
+
+ax3.plot(0, 0, 'bo', ms=1, label='Earth')
+ax3.plot(Mdata['X'], Mdata['Y'], marker ='',ms=3, c='black', label='Moon')
+colorPlot = ax3.scatter(IMdata['X'], IMdata['Y'], marker ='.', s=4, c=speed,cmap = 'jet')
+ax3.set_xlabel('X [km]')
+ax3.set_ylabel('Y [km]')
+ax3.set_title('IM-1 speed plot')
+ax3.set_aspect('equal')
+ax3.grid()
+
+
+plt.colorbar(colorPlot)
+
+
 plt.show()
